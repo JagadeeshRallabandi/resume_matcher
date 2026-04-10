d0ce1bf6cebfb2a15003b85b21f439112200460e
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2026-04-10 21:58:03 +0530

    skills_section_updated

---
+++ data/skills.py
@@ -0,0 +1,11 @@
+SKILLS = [
+    "java", "python", "c++", "c", "javascript", "typescript",
+    "react", "angular", "next.js",
+    "node.js", "express", "nestjs",
+    "mysql", "postgresql", "mongodb",
+    "docker", "kubernetes", "aws",
+    "rest api", "graphql",
+    "prisma", "redis",
+    "git", "ci/cd",
+    "html", "css"
+]
